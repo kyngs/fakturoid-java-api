4cd3c89b444373b0648d3da89b3910b6cbc7f4ad
kyngs <kyngs@users.noreply.github.com>
2024-03-16 21:05:26 +0100

    Add InvoicesClient#downloadInvoice

--- common/src/main/java/xyz/kyngs/fakturoid/api/v3/client/invoices/InvoicesClient.java
+++ common/src/main/java/xyz/kyngs/fakturoid/api/v3/client/invoices/InvoicesClient.java
@@ -2,6 +2,8 @@ package xyz.kyngs.fakturoid.api.v3.client.invoices;
 
 import xyz.kyngs.fakturoid.api.v3.model.invoice.Invoice;
 
+import java.io.File;
+
 public interface InvoicesClient {
     /**
      * Returns the invoice with the given id
@@ -9,4 +11,40 @@ public interface InvoicesClient {
      * @return the invoice with the given id, or null if it does not exist
      */
     Invoice getInvoice(int id);
+
+    /**
+     * Downloads the PDF of the invoice with the given id to the given file
+     * @param destination the file to download the invoice to
+     * @param id id of the invoice
+     * @return SUCCESS if the download was successful, GENERATING if the invoice is still being generated, NOT_FOUND if the invoice does not exist
+     */
+    DownloadInvoiceResult downloadInvoice(File destination, int id);
+
+    /**
+     * Downloads the PDF of the given invoice to the given file
+     * @param destination the file to download the invoice to
+     * @param invoice the invoice to download
+     * @return SUCCESS if the download was successful, GENERATING if the invoice is still being generated, NOT_FOUND if the invoice does not exist
+     */
+    default DownloadInvoiceResult downloadInvoice(File destination, Invoice invoice) {
+        return downloadInvoice(destination, invoice.getId());
+    }
+
+    /**
+     * Result of the downloadInvoice method
+     */
+    enum DownloadInvoiceResult {
+        /**
+         * The download was successful
+         */
+        SUCCESS,
+        /**
+         * The invoice is still being generated
+         */
+        GENERATING,
+        /**
+         * The invoice does not exist
+         */
+        NOT_FOUND
+    }
 }
--- okhttp4-client/src/main/java/xyz/kyngs/fakturoid/api/v3/impl/okhttp4/client/invoices/OKHTTP4InvoicesClient.java
+++ okhttp4-client/src/main/java/xyz/kyngs/fakturoid/api/v3/impl/okhttp4/client/invoices/OKHTTP4InvoicesClient.java
@@ -3,10 +3,10 @@ package xyz.kyngs.fakturoid.api.v3.impl.okhttp4.client.invoices;
 import xyz.kyngs.fakturoid.api.v3.client.invoices.InvoicesClient;
 import xyz.kyngs.fakturoid.api.v3.impl.okhttp4.OKHTTP4FakturoidClient;
 import xyz.kyngs.fakturoid.api.v3.impl.okhttp4.client.OKHTTP4AbstractClient;
-import xyz.kyngs.fakturoid.api.v3.model.auth.OAuthToken;
 import xyz.kyngs.fakturoid.api.v3.model.invoice.Invoice;
 
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
 
 public class OKHTTP4InvoicesClient extends OKHTTP4AbstractClient implements InvoicesClient {
     public OKHTTP4InvoicesClient(OKHTTP4FakturoidClient fakturoidClient, String account) {
@@ -16,5 +16,39 @@ public class OKHTTP4InvoicesClient extends OKHTTP4AbstractClient implements Invo
     @Override
     public Invoice getInvoice(int id) {
         return execute(createRequest("/accounts/" + account + "/invoices/" + id + ".json").get(), Invoice.class);
+    }
+
+    @Override
+    public DownloadInvoiceResult downloadInvoice(File destination, int id) {
+        if (destination.exists()) {
+            throw new IllegalArgumentException("Destination file already exists");
+        }
+
+        try (var res = execute(createRequest("/accounts/" + account + "/invoices/" + id + "/download.pdf").get())) {
+            switch (res.code()) {
+                case 200 -> {
+                    try (var body = res.body()) {
+                        if (!destination.createNewFile()) {
+                            throw new IOException("Could not create destination file");
+                        }
+
+                        var sink = okio.Okio.buffer(okio.Okio.sink(destination));
+                        assert body != null;
+                        sink.writeAll(body.source());
+                        sink.close();
+                        return DownloadInvoiceResult.SUCCESS;
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                case 404 -> {
+                    return DownloadInvoiceResult.NOT_FOUND;
+                }
+                case 204 -> {
+                    return DownloadInvoiceResult.GENERATING;
+                }
+            }
+        }
+        throw new RuntimeException("Failed to download invoice"); // Should never happen
     }
 }
